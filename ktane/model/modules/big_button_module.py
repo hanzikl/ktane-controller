3116e76cd56f2c4348df99427c52db1c8ea065e9
Jan Lopata <jan.lopata@rohlik.cz>
2018-09-30 13:51:13 +0200

    Big button - translation to command and tests finished

--- ktane/model/modules/big_button_module.py
+++ ktane/model/modules/big_button_module.py
@@ -1,3 +1,4 @@
+import random
 from enum import Enum
 
 from ktane.model.indicators import Indicators
@@ -22,6 +23,43 @@ class ButtonText(Enum):
 class CorrectAction(Enum):
     PRESS_AND_RELEASE = 0
     PRESS_AND_HOLD = 1
+
+
+class StripColor(Enum):
+    BLUE = 'blue'
+    YELLOW = 'yellow'
+    WHITE = 'white'
+    RED = 'red'
+
+
+def get_correct_digit(strip_color: StripColor):
+    if strip_color is None:
+        return None
+
+    if strip_color == StripColor.BLUE:
+        return 4
+
+    if strip_color == StripColor.WHITE:
+        return 1
+
+    if strip_color == StripColor.YELLOW:
+        return 5
+
+    return 1
+
+
+def translate_strip_color_to_data(color: StripColor):
+    if color == StripColor.BLUE:
+        return 1
+    if color == StripColor.YELLOW:
+        return 2
+    if color == StripColor.WHITE:
+        return 3
+    if color == StripColor.RED:
+        return 4
+
+    # fallback
+    return 0
 
 
 class BigButtonModule(AbstractModule):
@@ -57,10 +95,41 @@ class BigButtonModule(AbstractModule):
 
         return CorrectAction.PRESS_AND_HOLD
 
+    def get_correct_digit(self, strip_color):
+
+        return get_correct_digit(strip_color)
+
     def translate_to_commands(self):
         commands = super().translate_to_commands()
 
+        data_values = []
+        for strip_color in self.strip_colors:
+            if strip_color is None:
+                data_values.append(0)
+                continue
+
+            color_data = translate_strip_color_to_data(strip_color)
+            correct_digit = get_correct_digit(strip_color)
+            data_value = color_data * 16 + correct_digit
+            data_values.append(data_value)
+
+        data = " ".join([str(x) for x in data_values])
+        commands.append("SMD {} 0 {} 0 0".format(self.number, data))
+
         return commands
+
+    def get_random_strip_colors(self):
+
+        if self.get_correct_behaviour() == CorrectAction.PRESS_AND_RELEASE:
+            return [None, None, None]
+
+        result = []
+        all_strip_colors = [x for x in StripColor]
+        for i in range(3):
+            strip_color = random.choice(all_strip_colors)
+            result.append(strip_color)
+
+        return result
 
     def __init__(self, model):
         super().__init__(model)
@@ -69,3 +138,4 @@ class BigButtonModule(AbstractModule):
         self.state = ModuleState.ARMED
         self.color = None
         self.text = None
+        self.strip_colors = []
--- ktane/tests/test_big_button.py
+++ ktane/tests/test_big_button.py
@@ -3,7 +3,7 @@ import sys, os
 from ktane.model import Indicators
 
 sys.path.append(os.path.abspath('..'))
-from model.modules.big_button_module import BigButtonModule, ButtonColor, ButtonText
+from model.modules.big_button_module import BigButtonModule, ButtonColor, ButtonText, StripColor
 from model import Model
 import unittest
 
@@ -26,8 +26,8 @@ class KnownCorrectBehaviourValues(unittest.TestCase):
         ('yellow', 'detonate', 3, [], 0),
     )
 
-    def test_get_correct_wire_known_values(self):
-        """get_correct_wire should give known result for known value"""
+    def test_get_correct_behaviour_known_values(self):
+        """get_correct_behaviour should give known result for known value"""
         for color, text, batteries_count, indicators, behaviour in self.known_values_rules:
             ktane_model = Model()
             ktane_model.indicators = [Indicators(x) for x in indicators]
@@ -40,15 +40,55 @@ class KnownCorrectBehaviourValues(unittest.TestCase):
             self.assertEqual(behaviour, result.value)
 
 
+class KnownStripColorAndDigitValues(unittest.TestCase):
+    """ known strip colors"""
+    known_strip_colors_and_digits_rules = (
+        # all 4 rules
+        ('blue', 4),
+        ('white', 1),
+        ('yellow', 5),
+        ('red', 1),
+    )
+
+    def test_get_correct_digit_known_values(self):
+        """get_correct_digit should give known result for known value"""
+        for color, digit in self.known_strip_colors_and_digits_rules:
+            ktane_model = Model()
+            big_button_module = BigButtonModule(ktane_model)
+
+            result = big_button_module.get_correct_digit(StripColor(color))
+            self.assertEqual(digit, result)
+
+
 class TranslateCommandOnKnownValues(unittest.TestCase):
     known_values_translations = (
+        ('blue', 'abort', 3, ['frk'], ['white', 'red', 'blue'], 0,
+         ['SMD 0 0 49 65 20 0 0']),
+        ('red', 'detonate', 2, [], [None, None, None], 8,
+         ['SMD 8 0 0 0 0 0 0']),
+        ('red', 'abort', 2, ['frk', 'car'], ['blue', 'red', 'yellow'], 3,
+         ['SMD 3 0 20 65 37 0 0']),
     )
 
     def test_translations(self):
         """translate_to_commands should contain known result for given know module settings"""
 
-        # TODO
-        pass
+        for color, text, batteries, indicators, strip_colors, module_number, translation \
+                in self.known_values_translations:
+            ktane_model = Model()
+            ktane_model.indicators = [Indicators(x) for x in indicators]
+            ktane_model.batteries_count = batteries
+
+            module = BigButtonModule(ktane_model)
+            module.color = ButtonColor(color)
+            module.text = ButtonText(text)
+            module.number = module_number
+            module.strip_colors = [(lambda x: None if x is None else StripColor(x))(x) for x in strip_colors]
+
+            result = module.translate_to_commands()
+
+            for item in translation:
+                self.assertTrue(item in result, "{} does not contain '{}'".format(result, item))
 
 
 if __name__ == "__main__":
